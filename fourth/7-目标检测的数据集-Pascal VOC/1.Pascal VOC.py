bd3ea684b6d366fa41fc9dfb90cffe867fc8d49b
Liu. <3176408439@qq.com>
2026-03-12 20:28:19 +0800

    fourth

---
+++ fourth/7-目标检测的数据集-Pascal VOC/1.Pascal VOC.py
@@ -0,0 +1,27 @@
+''''''
+'''
+PASCAL VOC 数据集
+是计算机视觉领域中广泛使用的一个标准数据集,用于目标检测,图像分割,图像分类,动作识别等任务
+
+PASCAL VOC任务
+1. 目标检测：识别图像中的所有目标，并给出每个目标的类别和边界框。
+2. 图像分类：给出整幅图像的类别标签。
+3. 语义分割：对图像中的每个像素进行分类，标注其所属类别。
+4. 动作识别：识别图像中人物的动作类别。
+
+数据集标注工具
+LabelImg：https://github.com/HumanSignal/labelImg
+Labelme：https://github.com/labelmeai/labelme
+Label Studio：https://github.com/HumanSignal/label-studio
+CVAT: https://github.com/cvat-ai/cvat
+
+
+
+PASCAL VOC 目标检测领域的入门级标杆数据集
+
+标注类型:
+    边界框(目标检测)
+    类别标签(分类)
+    语义分割标签
+'''
+1
